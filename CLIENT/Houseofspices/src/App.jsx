3d5e035d3f9f14b92df5a69fd04646d67fd37971
riserishitha <rishitha.n@kalvium.community>
2024-04-26 17:14:52 +0530

    adding reactapp

---
+++ CLIENT/Houseofspices/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>HOUSE OF SPICES</h1>
+    <img src="https://www.tastingtable.com/img/gallery/tips-you-need-when-cooking-with-spices/intro-1683560012.jpg" alt="" />
+    </>
+  )
+}
+
+export default App
