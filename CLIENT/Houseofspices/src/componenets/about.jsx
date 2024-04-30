7e5da3ce5f290123117c69c896a034c14f394511
riserishitha <rishitha.n@kalvium.community>
2024-04-30 22:13:49 +0530

    addingcomponents

---
+++ CLIENT/Houseofspices/src/componenets/about.jsx
@@ -0,0 +1,10 @@
+function Aboutpage (){
+    return(
+        <div>
+<h1>
+    Contain content related to the app.
+</h1>
+        </div>
+    )
+}
+export default Aboutpage;
---
+++ CLIENT/Houseofspices/src/componenets/login.jsx
@@ -0,0 +1,25 @@
+function Loginpage (){
+    return(
+        <div>
+            <div>
+            <input type="text" placeholder="Full Name"/>
+            </div>
+            <div>
+            <input type="text" placeholder="Nick Name"/>
+            </div>
+            <div>
+            <input type="text" placeholder="Nick Name"/>
+            </div>
+            <div>
+            <input type="text" placeholder="Email Id"/>
+            </div>
+            <div>
+            <input type="text" placeholder="Phone Number"/>
+            </div>
+            <div>
+            <button>Login</button>
+            </div>
+        </div>
+    )
+}
+export default Loginpage;
