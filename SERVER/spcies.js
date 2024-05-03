021a50e0cba85dbaf8d46a72735572baf1519e37
riserishitha <rishitha.n@kalvium.community>
2024-05-03 20:24:37 +0530

    adding entities

---
+++ SERVER/spcies.js
@@ -0,0 +1,123 @@
+const Dataspices = [
+    {
+        spice: "Saffron",
+        rarity: "Rare",
+        commonAvailability: "Not Commonly Available",
+        health: "Rich in antioxidants, may improve mood and reduce PMS symptoms.",
+        image: "https://images.herzindagi.info/image/2023/Aug/grow-saffron-at-home-at-half-price.jpg"
+    },
+    {
+        spice: "Truffle",
+        rarity: "Rare",
+        commonAvailability: "Not Commonly Available",
+        health: "Rich in nutrients, may boost immune system.",
+        image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2023/3/03/black-truffles-on-board.jpg.rend.hgtvcom.1280.1280.suffix/1677867246448.jpeg"
+    },
+    {
+        spice: "Wasabi",
+        rarity: "Rare",
+        commonAvailability: "Not Commonly Available",
+        health: "Contains anti-inflammatory compounds, may aid digestion.",
+        image: "https://www.seriouseats.com/thmb/ZguiKK5SjTzF5MuUl2PYpROoOpg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/__opt__aboutcom__coeus__resources__content_migration__serious_eats__seriouseats.com__images__2014__03__20140316-wasabi-root-99c131d016ed4e0889ebbda2e9520b91.jpg"
+    },
+    {
+        spice: "Cardamom",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Aids digestion, freshens breath, may lower blood pressure.",
+        image: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/cardamom-benefits-1296x728-feature.jpg?w=1155&h=1528"
+    },
+    {
+        spice: "Vanilla",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Antioxidant properties, may reduce inflammation.",
+        image: "https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL3ZhbmlsbGEtZXhwZW5zaXZlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fX19"
+    },
+    {
+        spice: "Cinnamon",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "May lower blood sugar levels, has anti-inflammatory properties.",
+        image: "https://images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-cinnamon-1296x728.jpg"
+    },
+    {
+        spice: "Turmeric",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Powerful anti-inflammatory, may improve brain function.",
+        image: "https://static.toiimg.com/thumb/msid-107728574,width-400,resizemode-4/107728574.jpg"
+    },
+    {
+        spice: "Cloves",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Rich in antioxidants, may improve liver health.",
+        image: "https://images.healthshots.com/healthshots/en/uploads/2023/11/22175536/clove.jpg"
+    },
+    {
+        spice: "Ginger",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Anti-inflammatory, may reduce muscle pain and soreness.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs_9p0h_loT-3Use0M493sjtk6WPcrOCf_8MWs6KSBzQ&s"
+    },
+    {
+        spice: "Black Pepper",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Improves digestion, may enhance nutrient absorption.",
+        image: "https://media.post.rvohealth.io/wp-content/uploads/2020/08/black-pepper-benefits-732x549-thumbnail.jpg"
+    },
+    {
+        spice: "Nutmeg",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Contains compounds with antibacterial properties.",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbkn4bXMYscJtLCIAjaMhB0odIc637GQirN-b8AIxIeGLooJqI2Jj6YxnQN-DM_U8yLUw&usqp=CAU"
+    },
+    {
+        spice: "Fenugreek",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "May reduce appetite and lower blood sugar levels.",
+        image: "https://static.toiimg.com/thumb/imgsize-23456,msid-108266535,width-600,resizemode-4/108266535.jpg"
+    },
+    {
+        spice: "Star Anise",
+        rarity: "Rare",
+        commonAvailability: "Commonly Available",
+        health: "Rich in antioxidants, may promote heart health.",
+        image: "https://www.wayanadgreenfresh.com/cdn/shop/files/pexels-pixabay-247113_1024x1024@2x.jpg?v=1686923956"
+    },
+    {
+        spice: "Cumin",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Improves digestion, may help control blood sugar.",
+        image: "https://www.greendna.in/cdn/shop/products/cumin-powder-benefits_1200x1200.jpg?v=1561041069"
+    },
+    {
+        spice: "Juniper Berries",
+        rarity: "Rare",
+        commonAvailability: "Not Commonly Available",
+        health: "May have anti-inflammatory and antibacterial properties.",
+        image: "https://www.growforagecookferment.com/wp-content/uploads/2020/11/juniper-berries-wooden-spoon.jpg"
+    },
+    {
+        spice: "Paprika",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "Rich in vitamins and antioxidants.",
+        image: "https://i.etsystatic.com/14051632/r/il/f1ea65/1528109042/il_570xN.1528109042_g9m7.jpg"
+    },
+    {
+        spice: "Mustard Seeds",
+        rarity: "Common",
+        commonAvailability: "Commonly Available",
+        health: "May help reduce inflammation and lower cholesterol.",
+        image: "https://rukminim2.flixcart.com/image/850/1000/xif0q/plant-seed/1/m/t/100-whole-black-mustard-seeds-small-sarson-small-100g-original-imagkpkgb3hh4kdh.jpeg?q=90&crop=false"
+    }
+];
+
+module.exports = Dataspices;
