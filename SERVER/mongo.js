415aedafb111ff76f1e2f357b7d8b6ede31d1251
riserishitha <rishitha.n@kalvium.community>
2024-05-07 18:24:13 +0530

    creating schema and showing data in db

--- SERVER/mongo.js
+++ SERVER/mongo.js
@@ -1,13 +1,33 @@
 const mongoose = require("mongoose");
-const dot = require("dotenv")
-dot.config()
+const dot = require("dotenv");
+const data = require("./spcies");
+dot.config();
+
 async function backEnd() {
     try {
-        await mongoose.connect(process.env.connectdb);
-        console.log("Connected to mongoose");
+        await mongoose.connect(process.env.connectdb, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to MongoDB");
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
 }
 
-module.exports = backEnd;
+const spicesSchema = mongoose.Schema({
+    spice: String,
+    image: String,
+    rarity: String,
+    commonAvailability: String,
+    health: String,
+});
+
+const Spice = mongoose.model("Spice", spicesSchema);
+
+Spice.insertMany(data)
+    .then(() => console.log("Data sent"))
+    .catch((err) => console.error("Error sending data:", err));
+
+module.exports = {
+    connectdb: backEnd,
+    model: Spice
+};
+
