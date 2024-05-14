c6f0345607f38feeda7a0fab6f7d5563880dee3d
riserishitha <rishitha.n@kalvium.community>
2024-05-14 17:52:38 +0530

    get request

--- CLIENT/Houseofspices/src/componenets/front.jsx
+++ CLIENT/Houseofspices/src/componenets/front.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import Dummydata from "./dummy";
-
+import FetchData from "./getroute";
 function Frontpage (){
-    const [count, setCount] = useState(0);
-    const increase = () => {
-        setCount(count + 1);
-      };
-    
-      const decrease = () => {
-        if (count > 0) {
-          setCount(count - 1);
-        }
-      };
     return(
         <div>
             <div className="navbar" >
@@ -34,16 +22,9 @@ function Frontpage (){
             </div>
             </div>
             <div>
-              
             <div>
-      <h2>Spice: {Dummydata.spice}</h2>
-      <img src={Dummydata.image} width="500px"/>
-      <h4>Health: {Dummydata.health}</h4>
-      <h4>Rarity: {Dummydata.rarity}</h4>
-      <h4>CommonAvailability: {Dummydata.commonAvailability}</h4>
-    </div><button onClick={decrease}>-</button>
-            <span>{count}</span>
-          <button onClick={increase}>+</button>
+              <FetchData/>
+            </div>
             </div>
         </div>
     )
